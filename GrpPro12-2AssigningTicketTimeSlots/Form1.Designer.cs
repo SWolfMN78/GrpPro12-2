de6faab5125544836c515e5ca315404e250e27cf
Wolfknightx <Wolfknightx@Gmail.com>
2015-11-22 11:14:09 -0600

    Minor update to the form labels and buttons to give them their variable information.

--- GrpPro12-2AssigningTicketTimeSlots/Form1.Designer.cs
+++ GrpPro12-2AssigningTicketTimeSlots/Form1.Designer.cs
@@ -28,51 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblTicketCountOfGuest = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnIssueTicket = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lstTicketDisplayInfo = new System.Windows.Forms.ListBox();
+            this.lblTicketsOutstanding = new System.Windows.Forms.Label();
+            this.lblTicketTime = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnOptions
             // 
-            this.button1.Location = new System.Drawing.Point(16, 400);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Options";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnOptions.Location = new System.Drawing.Point(16, 400);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Size = new System.Drawing.Size(96, 23);
+            this.btnOptions.TabIndex = 0;
+            this.btnOptions.Text = "Options";
+            this.btnOptions.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnExit
             // 
-            this.button2.Location = new System.Drawing.Point(200, 400);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(200, 400);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblTicketCountOfGuest
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "1 - 5";
+            this.lblTicketCountOfGuest.AutoSize = true;
+            this.lblTicketCountOfGuest.Location = new System.Drawing.Point(24, 32);
+            this.lblTicketCountOfGuest.Name = "lblTicketCountOfGuest";
+            this.lblTicketCountOfGuest.Size = new System.Drawing.Size(28, 13);
+            this.lblTicketCountOfGuest.TabIndex = 2;
+            this.lblTicketCountOfGuest.Text = "1 - 5";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblTicketCountOfGuest);
             this.groupBox1.Location = new System.Drawing.Point(16, 16);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(280, 64);
@@ -98,20 +99,20 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Next availalbe entry: ";
             // 
-            // button3
+            // btnIssueTicket
             // 
-            this.button3.Location = new System.Drawing.Point(16, 96);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Issue Ticket";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnIssueTicket.Location = new System.Drawing.Point(16, 96);
+            this.btnIssueTicket.Name = "btnIssueTicket";
+            this.btnIssueTicket.Size = new System.Drawing.Size(112, 23);
+            this.btnIssueTicket.TabIndex = 6;
+            this.btnIssueTicket.Text = "Issue Ticket";
+            this.btnIssueTicket.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.lblTicketTime);
+            this.groupBox2.Controls.Add(this.lblTicketsOutstanding);
+            this.groupBox2.Controls.Add(this.btnIssueTicket);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Location = new System.Drawing.Point(16, 88);
@@ -121,42 +122,43 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
             // 
-            // listBox1
+            // lstTicketDisplayInfo
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(16, 232);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(280, 147);
-            this.listBox1.TabIndex = 8;
+            this.lstTicketDisplayInfo.FormattingEnabled = true;
+            this.lstTicketDisplayInfo.Location = new System.Drawing.Point(16, 232);
+            this.lstTicketDisplayInfo.Name = "lstTicketDisplayInfo";
+            this.lstTicketDisplayInfo.Size = new System.Drawing.Size(280, 147);
+            this.lstTicketDisplayInfo.TabIndex = 8;
             // 
-            // label4
+            // lblTicketsOutstanding
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(160, 32);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
+            this.lblTicketsOutstanding.AutoSize = true;
+            this.lblTicketsOutstanding.Location = new System.Drawing.Point(160, 32);
+            this.lblTicketsOutstanding.Name = "lblTicketsOutstanding";
+            this.lblTicketsOutstanding.Size = new System.Drawing.Size(78, 13);
+            this.lblTicketsOutstanding.TabIndex = 7;
+            this.lblTicketsOutstanding.Text = "# Place Holder";
             // 
-            // label5
+            // lblTicketTime
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(160, 64);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "label5";
+            this.lblTicketTime.AutoSize = true;
+            this.lblTicketTime.Location = new System.Drawing.Point(160, 64);
+            this.lblTicketTime.Name = "lblTicketTime";
+            this.lblTicketTime.Size = new System.Drawing.Size(94, 13);
+            this.lblTicketTime.TabIndex = 8;
+            this.lblTicketTime.Text = "Time Place Holder";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
             this.ClientSize = new System.Drawing.Size(313, 434);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lstTicketDisplayInfo);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOptions);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -170,17 +172,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblTicketCountOfGuest;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnIssueTicket;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstTicketDisplayInfo;
+        private System.Windows.Forms.Label lblTicketTime;
+        private System.Windows.Forms.Label lblTicketsOutstanding;
     }
 }
 
