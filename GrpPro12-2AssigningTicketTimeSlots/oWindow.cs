ce46f47be92c48808554f929bd019425c9995029
Robert Masen <r.f.masen@gmail.com>
2015-11-27 14:57:38 -0600

    Construtors added to oWindow
    Added empty and parameterized constructors to the oWindow class. Made properties readonly. Added method for updating properties.

--- GrpPro12-2AssigningTicketTimeSlots/oWindow.cs
+++ GrpPro12-2AssigningTicketTimeSlots/oWindow.cs
@@ -6,11 +6,58 @@ using System.Threading.Tasks;
 
 namespace GrpPro12_2AssigningTicketTimeSlots
 {
-    // object which holds information of what is going to go into the window.
+    
     class oWindow
     {
-        public DateTime StartTime { get; set; }
-        public int People { get; set; }
-        public DateTime Duration { get; set; }
+        public DateTime StartTime { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public int MaxRiders { get; private set; }
+        public int RidersRemaining { get; private set; }
+        
+        //generates a defauld window with a starttime of 
+        // now, a MaxRiders of 5, and a duration of 5 minutes
+        public oWindow()
+        {
+            StartTime = Now();
+            Duration = new TimeSpan(0,5,0);
+            MaxRiders = 5;
+            RidersRemaining = MaxRiders;
+        }
+        
+        public oWindow(DateTime start, int people, int duration)
+        {
+            this.StartTime = start;
+            this.Duration = new TimeSpan(0,duration,0);
+            this.MaxRiders = people;
+            this.RidersRemaining = this.MaxRiders;
+        }
+        
+        private void ResetWindow()
+        {
+            StartTime.Add(Duration)
+            ResetRiders()
+        }
+        
+        private void ResetRiders()
+        {
+            this.RidersRemaining = MaxRiders;   
+        }
+        
+        //this class checks to see how many riders are remaining, if it is 0
+        //then it will reset the window to a new time genenerated with the duration property
+        //with the maximum number of riders otherwise it will just reduce the RidersRemaining
+        //property by 1.
+        public void RemoveRider()
+        {
+            if (RidersRemaining > 0)
+            {
+                this.RidersRemaiing -= 1;
+            }
+            else
+            {
+                ResetWindow()
+            }
+        }
+        
     }
 }
