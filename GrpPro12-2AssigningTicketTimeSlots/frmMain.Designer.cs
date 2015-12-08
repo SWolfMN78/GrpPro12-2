ca1c9e9f1760c0c935482b6503315d6ee61f86e5
Wolfknightx <Wolfknightx@Gmail.com>
2015-12-08 17:25:05 -0600

    My current material

--- GrpPro12-2AssigningTicketTimeSlots/frmMain.Designer.cs
+++ GrpPro12-2AssigningTicketTimeSlots/frmMain.Designer.cs
@@ -58,7 +58,7 @@
             // btnExit
             // 
             this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnExit.Location = new System.Drawing.Point(200, 400);
+            this.btnExit.Location = new System.Drawing.Point(192, 400);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(99, 23);
             this.btnExit.TabIndex = 1;
